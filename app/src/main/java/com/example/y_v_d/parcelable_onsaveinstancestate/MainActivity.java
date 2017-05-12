f0b8fba543ed6bacac2cb382f0dd953366bece32
caemooshin <mooshin4@gmail.com>
2017-05-12 20:13:55 +0700

    Initial commit

---
+++ app/src/main/java/com/example/y_v_d/parcelable_onsaveinstancestate/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.y_v_d.parcelable_onsaveinstancestate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
